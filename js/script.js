8dcf3093a6a73b053528ca7b8acbe329da917337
Nick Davies <nickdavies100@gmail.com>
2020-03-24 15:55:10 -0700

    Latest

--- js/script.js
+++ js/script.js
@@ -2,32 +2,71 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
-
+/* - Movie quotes taken from 
+https://www.marieclaire.com/culture/g19738923/most-iconic-movie-quotes-year-you-were-born/
+*/
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 
+ //`quotes` array 
+var printedOutput;
+ var quotes = [
+  { Quote: "Frankly my dear, I don't give a damn",
+    Source: "Clark Gable",
+    Citation: "Gone with the Wind",
+    Year: 1939
+  },
+  { Quote: "Here's looking at you, kid",
+    Source: "Humphrey Bogart",
+    Citation: "Casablanca",
+    Year: 1942
+  },
+  { Quote: "Fashten your seat belts. It's going to be a bumpy night.",
+    Source: "Margo Channing",
+    Citation: "All About Eve",
+    Year: 1950
+  },
+  { Quote: "Bond. James Bond",
+    Source: "Sean Connery",
+    Citation: "James Bond",
+    Year: 1962
+  },
+  { Quote: "Don’t you know a sarcasm when you hear it?",
+    Source: "Lucy",
+    Citation: "A Charlie Brown Christmas",
+    Year: 1965
+  }
+]
 
+//Checking output in console
+/*for (var i = 0; i<=4; i+= 1){
+  console.log(quotes[i]);
+}
+*/
 
-/***
- * `getRandomQuote` function
-***/
+// * `getRandomQuote` function
+function getRandomQuote(quotes) {
+  console.log(quotes.length);
 
+  var number = Math.floor(Math.random() * (quotes.length) + 1);
+  var chosenQuote = quotes[number];
+  console.log(chosenQuote);
+  return chosenQuote; 
+}
 
+getRandomQuote();
 
-/***
- * `printQuote` function
-***/
+// `printQuote` function
+function printQuote(){
+  var choice = quotes[getRandomQuote()];
 
-
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
